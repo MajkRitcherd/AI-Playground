6d7c154c6b0b78973198eeba4d05c93375a64fbe
MajkRitcherd <misutak87@gmail.com>
2026-08-14 13:41:39 +0200

    feat: Add LearningEndpoint with test route, http for testing requests

---
+++ Web/AI-Playground/AI-Playground.Api/Endpoints/LearningEndpoints.cs
@@ -0,0 +1,19 @@
+﻿namespace AI_Playground.Web.Endpoints
+{
+    public static class LearningEndpoints
+    {
+        public static RouteGroupBuilder MapLearningRoutes(this IEndpointRouteBuilder builder)
+        {
+            var group = builder
+                .MapGroup("/api/learning");
+
+            // Initially left empty before actual routes
+            group.MapGet("/test", (string name = "world") =>
+            {
+                return Results.Ok($"Hello {name}");
+            });
+
+            return group;
+        }
+    }
+}
--- Web/AI-Playground/AI-Playground.Api/Program.cs
+++ Web/AI-Playground/AI-Playground.Api/Program.cs
@@ -1,3 +1,5 @@
+using AI_Playground.Web.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,10 +15,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.MapLearningRoutes();
+
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
